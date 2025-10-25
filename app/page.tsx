6ba0c15a02965840446652cf6e82d41d685d2f79
Flavianna Saldanha <flaviannasaldanha@gmail.com>
2025-10-25 15:10:35 -0300

    organizando projeto e add assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 const Page = () => {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <h1>Oi</h1>
+      <a href="">link</a>
+      <h2>texto 2</h2>
+    </div>
+  );
 };
 
 export default Page;
