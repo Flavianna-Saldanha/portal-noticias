01f9f73ea1945bdbfdf26c8c0ac6eeb672cd03e0
Flavianna Saldanha <flaviannasaldanha@gmail.com>
2025-10-25 14:40:19 -0300

    Iniciando o projeto

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Portal de Notícias",
+  description: "Site dedicado a notícias variadas.",
+  icons: {
+    icon: "/favicon.png",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
